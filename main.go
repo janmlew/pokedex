0324aeb2fc5c0655eb98b0d9080588abfe3d1814
Jan Lewiński <janmikolajlewinski@gmail.com>
2026-06-04 23:43:14 +0200

    simple REPL support added

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		words := cleanInput(scanner.Text())
+		if len(words) == 0 {
+			continue
+		}
+		fmt.Printf("Your command was: %s\n", words[0])
+	}
 }
